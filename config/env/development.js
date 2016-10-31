2d444723e1e4ef77b8fcc6274fe1da8949009a72
LUIS CAMILO JIMENEZ ALVAREZ <LCJIMENEZ@universidad.ean>
2016-10-31 00:22:06 -0500

    Ajustar callbackURL: "/oauth/google/callback" de server.js

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ module.exports = {
 	google: {
 		clientID: "404989508130-1lq87fnfopkuitch9hb4tfigj04a6la3.apps.googleusercontent.com",
 		clientSecret: "Cof-Hnq9tUj5a_RJxUgXeACd",
-		callbackURL: "http://localhost:3000/oauth/google/callback"
+		callbackURL: "/oauth/google/callback"
 	},
 	//se asegura que el correo de quien ingresa esté vinculado a la Universidad EAN
 	domain: "universidadean.edu.co"
